f6e8bc818f2693ee5c5238e3781d416f0ccf2d15
Billy <bryant4882@gmail.com>
2020-04-16 05:08:39 +0800

    prototype

---
+++ Prototype1/node2/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  consumer_key:         'Jb0OPHRNpQrv7PNiBU6XeCnnH',
+  consumer_secret:      'LosHoxwiKUq1q7CDJ5RlS6IyD1k3ZKl4Hss0loc6KqHNR9IVL4',
+  access_token:         '960832158460928000-IcOhbXPXsGRGQqINLj6Vc4gbp69sKHq',
+  access_token_secret:  'W0Y4n20hinaoO4AnYOR27ZHNikB2F8p9uFM1wsIXHGzkN',
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  //strictSSL:            true,     // optional - requires SSL certificates to be valid.
+
+
+}
